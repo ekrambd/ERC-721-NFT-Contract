9d835cbed66e0ea891597b01c637e7477aa3e24e
ekrambd <55123967+ekrambd@users.noreply.github.com>
2025-10-28 00:47:30 +0600

    Update NFTX.sol

--- NFTX.sol
+++ NFTX.sol
@@ -17,59 +17,55 @@ contract NFTX is ERC721URIStorage, Ownable, ReentrancyGuard {
     }
 
     mapping(uint256 => NFTData) public nftData;
-    mapping(uint256 => bool) private tokenExistsMapping;
+    mapping(uint256 => string) private tokenURIs;
+    mapping(uint256 => bool) private metadataExists;
+    mapping(uint256 => bool) public isMinted;
 
-    event NFTMinted(address indexed owner, uint256 indexed tokenId, string title, uint256 price);
-    event NFTUpdated(uint256 indexed tokenId, string title, string description, uint256 price, string imageURI);
-    event NFTDeleted(uint256 indexed tokenId);
+    event NFTAdded(uint256 indexed tokenId, string title, uint256 price);
+    event NFTMinted(address indexed user, uint256 indexed tokenId);
     event TransferEnabled(address indexed account);
 
-    constructor() ERC721("NFTX", "NFX") Ownable(msg.sender) {}
+    constructor() ERC721("NFTX", "NFX") Ownable(msg.sender){}
 
     function setTransfersEnabled(bool enabled) external onlyOwner {
         transfersEnabled = enabled;
         emit TransferEnabled(msg.sender);
     }
 
-    function mint(
+    function addNFTMetadata(
         string memory title,
         string memory description,
         string memory imageURI,
         uint256 price,
         string memory tokenURI
-    ) external payable nonReentrant {
-        require(msg.value >= price, "Insufficient Balance");
-        uint256 tokenId = nextTokenId;
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-        nftData[tokenId] = NFTData(title, description, imageURI, price);
-        tokenExistsMapping[tokenId] = true;
-        nextTokenId++;
-        emit NFTMinted(msg.sender, tokenId, title, price);
-    }
-
-    function tokenExists(uint256 tokenId) public view returns (bool) {
-        return tokenExistsMapping[tokenId];
-    }
-
-    function editNFT(
-        uint256 tokenId,
-        string memory newTitle,
-        string memory newDescription,
-        string memory newImageURI,
-        uint256 newPrice
     ) external onlyOwner {
-        require(tokenExists(tokenId), "NFT does not exist");
-        nftData[tokenId] = NFTData(newTitle, newDescription, newImageURI, newPrice);
-        emit NFTUpdated(tokenId, newTitle, newDescription, newPrice, newImageURI);
+        uint256 tokenId = nextTokenId;
+        nftData[tokenId] = NFTData(title, description, imageURI, price);
+        tokenURIs[tokenId] = tokenURI;
+        metadataExists[tokenId] = true;
+        nextTokenId++;
+
+        emit NFTAdded(tokenId, title, price);
     }
 
-    function deleteNFT(uint256 tokenId) external onlyOwner {
-        require(tokenExists(tokenId), "NFT does not exist");
-        delete nftData[tokenId];
-        _burn(tokenId);
-        tokenExistsMapping[tokenId] = false;
-        emit NFTDeleted(tokenId);
+    function mintNFT(uint256 tokenId) external payable nonReentrant {
+        require(metadataExists[tokenId], "NFT metadata not exists");
+        require(!isMinted[tokenId],"Already Minted");
+        NFTData memory nft = nftData[tokenId];
+        require(msg.value >= nft.price, "Insufficient MATIC");
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURIs[tokenId]);
+
+        isMinted[tokenId] = true;
+
+        emit NFTMinted(msg.sender, tokenId);
+    }
+
+    function withdrawFunds() external onlyOwner nonReentrant {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds to withdraw");
+        payable(owner()).transfer(balance);
     }
 
     function _update(
@@ -82,18 +78,5 @@ contract NFTX is ERC721URIStorage, Ownable, ReentrancyGuard {
             require(transfersEnabled, "Transfers are disabled");
         }
         return super._update(to, tokenId, auth);
-    }
-
-    function fullFundWithdraw() external onlyOwner nonReentrant {
-        uint256 balance = address(this).balance;
-        require(balance > 0, "No funds to withdraw");
-        payable(owner()).transfer(balance);
-    }
-
-    function withdrawAmount(uint256 amount) external onlyOwner nonReentrant {
-        uint256 balance = address(this).balance;
-        require(amount > 0, "Amount must be > 0");
-        require(amount <= balance, "Not enough balance");
-        payable(owner()).transfer(amount);
     }
 }
